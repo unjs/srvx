c726e402093aa0684b91e05b3329886ffa29fcd7
Pooya Parsa <pooya@pi0.io>
2025-04-01 21:23:00 +0200

    refactor: remove unused symbols

--- src/_node-compat/_common.ts
+++ src/_node-compat/_common.ts
@@ -1,9 +1,3 @@
 export const kNodeInspect = /* @__PURE__ */ Symbol.for(
   "nodejs.util.inspect.custom",
 );
-
-export const kResBody: unique symbol =
-  /* @__PURE__ */ Symbol.for("srvx.response.body");
-
-export const kResInit: unique symbol =
-  /* @__PURE__ */ Symbol.for("srvx.response.init");

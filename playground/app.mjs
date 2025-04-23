5e6f8863e3743168e3560c04d56c5ddef1d69d40
Pooya Parsa <pooya@pi0.io>
2025-04-23 11:21:11 +0200

    update playground

--- playground/app.mjs
+++ playground/app.mjs
@@ -2,11 +2,11 @@ import { serve } from "srvx";
 
 const server = serve({
   // protocol: "https",
-  tls: { cert: "server.crt", key: "server.key" },
+  // tls: { cert: "server.crt", key: "server.key" },
   port: 3000,
   fetch(_request) {
     return new Response(
-      `
+      /*html */ `
         <h1>👋 Hello there</h1>
         Learn more: <a href="https://srvx.h3.dev/" target="_blank">srvx.h3.dev</a>
       `,
@@ -14,6 +14,14 @@ const server = serve({
         headers: {
           "Content-Type": "text/html; charset=UTF-8",
         },
+      },
+    );
+  },
+  onError(error) {
+    return new Response(
+      /*html */ `<body style="background-color:blue;color:white;padding:2em;"><pre>${error.stack || error}</pre></body>`,
+      {
+        headers: { "Content-Type": "text/html" },
       },
     );
   },

b333bd48d4adee6952502129020a1fcd2c2c717f
Pooya Parsa <pooya@pi0.io>
2025-04-23 14:00:07 +0200

    chore: update playground sw example to use cdn

--- playground/sw/sw.mjs
+++ playground/sw/sw.mjs
@@ -1,10 +1,11 @@
-import { serve } from "../node_modules/srvx/dist/adapters/service-worker.mjs";
+// import { serve } from "../node_modules/srvx/dist/adapters/service-worker.mjs";
+import { serve } from "https://esm.sh/srvx@0.5";
 
 serve({
   serviceWorker: { url: import.meta.url },
   fetch(_request) {
     return new Response(
-      `
+      /*html*/ `
         <h1>👋 Hello there!</h1>
         Learn more: <a href="https://srvx.h3.dev/" target="_blank">srvx.h3.dev</a>
       `,

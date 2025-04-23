63a42b5e4ac12462fdacfde710d237aa02272693
Pooya Parsa <pooya@pi0.io>
2025-04-23 15:09:10 +0200

    fix(service-worker): minor fixes

--- playground/sw/sw.mjs
+++ playground/sw/sw.mjs
@@ -1,19 +1,11 @@
-// import { serve } from "../node_modules/srvx/dist/adapters/service-worker.mjs";
-import { serve } from "https://esm.sh/srvx@0.5";
+import { serve } from "../node_modules/srvx/dist/adapters/service-worker.mjs";
+// import { serve } from "https://esm.sh/srvx@0.5";
 
 serve({
   serviceWorker: { url: import.meta.url },
   fetch(_request) {
-    return new Response(
-      /*html*/ `
-        <h1>👋 Hello there!</h1>
-        Learn more: <a href="https://srvx.h3.dev/" target="_blank">srvx.h3.dev</a>
-      `,
-      {
-        headers: {
-          "Content-Type": "text/html; charset=UTF-8",
-        },
-      },
-    );
+    return new Response(`<h1>👋 Hello there!</h1>`, {
+      headers: { "Content-Type": "text/html; charset=UTF-8" },
+    });
   },
 });
--- src/adapters/service-worker.ts
+++ src/adapters/service-worker.ts
@@ -1,3 +1,4 @@
+/* eslint-disable unicorn/prefer-global-this */
 import type { Server, ServerOptions, ServerRequest } from "../types.ts";
 import { wrapFetch } from "../_plugin.ts";
 import { wrapFetchOnError } from "../_error.ts";
@@ -9,8 +10,10 @@ export type ServiceWorkerHandler = (
   event: FetchEvent,
 ) => Response | Promise<Response>;
 
-const isMainThread = () =>
-  typeof navigator !== "undefined" && "serviceWorker" in navigator;
+const isBrowserWindow =
+  typeof window !== "undefined" && typeof navigator !== "undefined";
+
+const isServiceWorker = typeof self !== "undefined" && "skipWaiting" in self;
 
 export function serve(options: ServerOptions): Server<ServiceWorkerHandler> {
   return new ServiceWorkerServer(options);
@@ -48,51 +51,58 @@ class ServiceWorkerServer implements Server<ServiceWorkerHandler> {
   }
 
   serve() {
-    if (isMainThread()) {
+    if (isBrowserWindow) {
+      if (!navigator.serviceWorker) {
+        throw new Error(
+          "Service worker is not supported in the current window.",
+        );
+      }
       const swURL = this.options.serviceWorker?.url;
       if (!swURL) {
         throw new Error(
           "Service worker URL is not provided. Please set the `serviceWorker.url` serve option or manually register.",
         );
       }
-      // Not a service worker, so self-register the service worker
+      // Self-register the service worker
       this.#listeningPromise = navigator.serviceWorker
         .register(swURL, {
           type: "module",
           scope: this.options.serviceWorker?.scope,
         })
         .then((registration) => {
-          registration.addEventListener("updatefound", () => {
+          if (registration.active) {
             location.replace(location.href);
-          });
+          } else {
+            registration.addEventListener("updatefound", () => {
+              location.replace(location.href);
+            });
+          }
         });
-      return;
+    } else if (isServiceWorker) {
+      // Listen for the 'fetch' event to handle requests
+      this.#fetchListener = async (event) => {
+        // skip if event url ends with file with extension
+        if (
+          /\/[^/]*\.[a-zA-Z0-9]+$/.test(new URL(event.request.url).pathname)
+        ) {
+          return;
+        }
+        const response = await this.fetch(event.request, event);
+        if (response.status !== 404) {
+          event.respondWith(response);
+        }
+      };
+
+      addEventListener("fetch", this.#fetchListener);
+
+      self.addEventListener("install", () => {
+        self.skipWaiting();
+      });
+
+      self.addEventListener("activate", () => {
+        self.clients?.claim?.();
+      });
     }
-
-    // Listen for the 'fetch' event to handle requests
-    this.#fetchListener = async (event) => {
-      // skip if event url ends with file with extension
-      if (/\/[^/]*\.[a-zA-Z0-9]+$/.test(new URL(event.request.url).pathname)) {
-        return;
-      }
-      const response = await this.fetch(event.request, event);
-      if (response.status !== 404) {
-        event.respondWith(response);
-      }
-    };
-    addEventListener("fetch", this.#fetchListener);
-
-    // Listen for the 'install' event to update the service worker
-    globalThis.addEventListener("install", () => {
-      // Force the waiting service worker to become active
-      (globalThis as any).skipWaiting();
-    });
-
-    // Listen for the 'activate' event to claim clients
-    globalThis.addEventListener("activate", (event) => {
-      // Take control of uncontrolled clients
-      (event as FetchEvent).waitUntil((globalThis as any).clients?.claim?.());
-    });
   }
 
   ready(): Promise<Server<ServiceWorkerHandler>> {
@@ -105,15 +115,15 @@ class ServiceWorkerServer implements Server<ServiceWorkerHandler> {
     }
 
     // unregister the service worker
-    if (isMainThread()) {
+    if (isBrowserWindow) {
       const registrations = await navigator.serviceWorker.getRegistrations();
       for (const registration of registrations) {
         if (registration.active) {
           await registration.unregister();
         }
       }
-    } else {
-      await (globalThis as any).registration.unregister();
+    } else if (isServiceWorker) {
+      await self.registration.unregister();
     }
   }
 }
